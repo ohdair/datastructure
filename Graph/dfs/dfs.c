db31c453f1a037d7124bc2f21931105c78fa3e58
ohdair <ohdiar@gmail.com>
2021-09-26 23:07:21 +0900

    h

--- Graph/dfs/dfs.c
+++ Graph/dfs/dfs.c
@@ -1,5 +1,18 @@
 #include "../linkedGraph/graph.h"
+#include "../../Stack/stack.h"
 
 void traversal_DFS(LinkedGraph *pGraph){
+    Stack *s = NULL;
+    ListNode *u = NULL;
     
+    s = init();
+    pGraph->pVertex[0] = USED;
+    u = &pGraph->ppAdjEdge[0]->headerNode;
+    ft_push(s, 0);
+    while (s){
+        pop(u);
+        for(){
+            
+        }
+    }
 }
